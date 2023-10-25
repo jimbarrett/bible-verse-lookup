73c1cf38d4a4a55312b83bda338eba3099b2726a
Jim Barrett <barrettj1971@gmail.com>
2023-10-25 11:24:25 -0400

    style change for verse #

--- components/Verse.jsx
+++ components/Verse.jsx
@@ -2,7 +2,7 @@ const Verse = ({ items, compared = false }) => {
   let out = items.map((verse) => {
     return (
       <article key={verse.id} className="pb-2">
-        <span className="font-bold italic text-xs text-gray-400 relative bottom-1">
+        <span className="font-bold italic underline text-xs text-blue-400 relative bottom-1">
           {/* TODO this needs to be cleaned up */}
           {verse.b !== items[0].b && verse.title_short + ":"}
           {verse.c !== items[0].c && verse.c + ":"}
